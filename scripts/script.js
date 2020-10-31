3d57c3168393d12bf83fc18dbbe3151dbed8a232
Gennadiy Serikov <serikov.gen@yandex.ru>
2020-10-31 03:49:29 +0300

    fix: all first review requirements fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,35 @@
-document.addEventListener('DOMContentLoaded', BindButtonEventListeners);
+let popup = document.querySelector('.popup');
+let popupForm = document.querySelector('.popup__form');
+let popupInputName = document.querySelector('.popup__field_text_name');
+let popupInputStatus = document.querySelector('.popup__field_text_status');
+let popupCloseButton = document.querySelector('.popup__close-button');
 
-function BindButtonEventListeners() {
-    let profileEditButton = document.querySelector('.profile__edit-button');
-    profileEditButton.addEventListener('click', OpenPopup);
+let profileName   = document.querySelector('.profile__name');
+let profileStatus = document.querySelector('.profile__status');
+let profileEditButton = document.querySelector('.profile__edit-button');
 
-    let formElement = document.querySelector('.popup__container');
-    formElement.addEventListener('submit', formSubmitHandler);
+function PopupFormSubmit (evt) {
+    evt.preventDefault();
+
+    profileName.textContent = popupInputName.value;
+    profileStatus.textContent = popupInputStatus.value;
+    ClosePopup();
 }
 
 function OpenPopup() {
-  let name = document.querySelector('.profile__name').textContent;
-  let job  = document.querySelector('.profile__status').textContent;
+  if(!popup.classList.contains('popup_opened')) {
+    popup.classList.add('popup_opened');
+  }
+}
 
-  document.querySelector('.popup__container input[name=profile__name]').value = name;
-  document.querySelector('.popup__container input[name=profile__status]').value = job;
-
-  let popup = document.querySelector('.popup');
-  popup.classList.add('popup_opened');
-
-  let profileCloseButton = document.querySelector('.popup__close-button');
-  profileCloseButton.addEventListener('click', function(){
-    let popup = document.querySelector('.popup');
+function ClosePopup() {
+  if(popup.classList.contains('popup_opened')) {
     popup.classList.remove('popup_opened');
-  });
+  }
 }
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-
-    // Находим поля формы в DOM
-    let nameTag = document.querySelector('.profile__name');
-    let jobTag  = document.querySelector('.profile__status');
-
-    let nameInput = document.querySelector('.popup__container input[name=profile__name]');
-    let jobInput = document.querySelector('.popup__container input[name=profile__status]');
-
-    nameTag.textContent = nameInput.value;
-    jobTag.textContent = jobInput.value;
-
-    document.querySelector('.popup__close-button').click();
-}
+document.addEventListener("DOMContentLoaded", function() {
+   profileEditButton.addEventListener('click', OpenPopup);
+   popupCloseButton.addEventListener('click', ClosePopup);
+   popupForm.addEventListener('submit', PopupFormSubmit);
+});
